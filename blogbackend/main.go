54c5889e9027fa96fbc79f6c556e127a2f8e5588
amrizal <amrizal0310@gmail.com>
2024-04-18 19:37:08 +0700

    Add files via upload
    add file

---
+++ blogbackend/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"blogbackend/database"
+	"blogbackend/routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	database.Connect()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+	app := fiber.New()
+	routes.Setup(app)
+	app.Listen(":" + port)
+
+}
